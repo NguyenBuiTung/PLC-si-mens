1c753a868288a984f0aa2eafba93bb6ed165faa9
Tung <nguyenbuitung4@gmail.com>
2024-10-07 15:03:13 +0700

    ff

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace S7_1200_with_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIpAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtWriteData = new System.Windows.Forms.TextBox();
+            this.txtReadData = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnReadFloat = new System.Windows.Forms.Button();
+            this.btnWriteFloat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIpAddress
+            // 
+            this.txtIpAddress.Location = new System.Drawing.Point(92, 82);
+            this.txtIpAddress.Name = "txtIpAddress";
+            this.txtIpAddress.Size = new System.Drawing.Size(194, 20);
+            this.txtIpAddress.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập IP kết nối";
+            // 
+            // txtWriteData
+            // 
+            this.txtWriteData.Location = new System.Drawing.Point(510, 145);
+            this.txtWriteData.Name = "txtWriteData";
+            this.txtWriteData.Size = new System.Drawing.Size(194, 20);
+            this.txtWriteData.TabIndex = 2;
+            // 
+            // txtReadData
+            // 
+            this.txtReadData.Location = new System.Drawing.Point(510, 81);
+            this.txtReadData.Name = "txtReadData";
+            this.txtReadData.ReadOnly = true;
+            this.txtReadData.Size = new System.Drawing.Size(194, 20);
+            this.txtReadData.TabIndex = 3;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackColor = System.Drawing.Color.Chartreuse;
+            this.btnConnect.Location = new System.Drawing.Point(92, 145);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(194, 67);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Kết nối";
+            this.btnConnect.UseVisualStyleBackColor = false;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click_1);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(745, 82);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 5;
+            this.btnRead.Text = "Đọc";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click_1);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(745, 145);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 6;
+            this.btnWrite.Text = "Ghi";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click_1);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(381, 48);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblStatus.TabIndex = 7;
+            this.lblStatus.Text = "label2";
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnDisconnect.Location = new System.Drawing.Point(92, 244);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(194, 67);
+            this.btnDisconnect.TabIndex = 8;
+            this.btnDisconnect.Text = " Ngắt kết nối";
+            this.btnDisconnect.UseVisualStyleBackColor = false;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnReadFloat
+            // 
+            this.btnReadFloat.Location = new System.Drawing.Point(745, 219);
+            this.btnReadFloat.Name = "btnReadFloat";
+            this.btnReadFloat.Size = new System.Drawing.Size(75, 23);
+            this.btnReadFloat.TabIndex = 9;
+            this.btnReadFloat.Text = "Đọc số thực";
+            this.btnReadFloat.UseVisualStyleBackColor = true;
+            this.btnReadFloat.Click += new System.EventHandler(this.btnReadFloat_Click);
+            // 
+            // btnWriteFloat
+            // 
+            this.btnWriteFloat.Location = new System.Drawing.Point(745, 288);
+            this.btnWriteFloat.Name = "btnWriteFloat";
+            this.btnWriteFloat.Size = new System.Drawing.Size(75, 23);
+            this.btnWriteFloat.TabIndex = 10;
+            this.btnWriteFloat.Text = "Ghi số thực";
+            this.btnWriteFloat.UseVisualStyleBackColor = true;
+            this.btnWriteFloat.Click += new System.EventHandler(this.btnWriteFloat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 598);
+            this.Controls.Add(this.btnWriteFloat);
+            this.Controls.Add(this.btnReadFloat);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtReadData);
+            this.Controls.Add(this.txtWriteData);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIpAddress);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIpAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtWriteData;
+        private System.Windows.Forms.TextBox txtReadData;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnReadFloat;
+        private System.Windows.Forms.Button btnWriteFloat;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Windows.Forms;
+using S7.Net;
+
+namespace S7_1200_with_CSharp
+{
+    public partial class Form1 : Form
+    {
+        Plc plc_s7_1200;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Các phần tử giao diện (UI) sẽ được thêm qua Form Designer (kéo thả trong giao diện thiết kế).
+        }
+
+        // Sự kiện Click của nút "Kết nối" (btnConnect) để kết nối tới PLC
+        private void btnConnect_Click_1(object sender, EventArgs e)
+        {
+            string ipAddress = txtIpAddress.Text; // Lấy địa chỉ IP từ TextBox mà người dùng nhập vào
+            try
+            {
+                // Tạo đối tượng PLC với loại CPU S71200, địa chỉ IP và Rack và Slot của PLC
+                plc_s7_1200 = new Plc(CpuType.S71200, ipAddress, 0, 1);
+                plc_s7_1200.Open(); // Mở kết nối tới PLC
+
+                // Kiểm tra nếu kết nối thành công
+                if (plc_s7_1200.IsConnected)
+                {
+                    lblStatus.Text = "Trạng thái: Đã kết nối"; // Hiển thị trạng thái kết nối
+                    MessageBox.Show("Kết nối thành công với PLC!"); // Thông báo thành công
+                }
+                else
+                {
+                    lblStatus.Text = "Trạng thái: Kết nối thất bại"; // Hiển thị trạng thái kết nối thất bại
+                    MessageBox.Show("Kết nối PLC thất bại."); // Thông báo thất bại
+                }
+            }
+            catch (Exception exp)
+            {
+                // Bắt lỗi nếu có ngoại lệ xảy ra trong quá trình kết nối
+                lblStatus.Text = "Trạng thái: Lỗi kết nối";
+                MessageBox.Show(exp.Message); // Hiển thị thông báo lỗi
+            }
+        }
+
+        // Phương thức kiểm tra kết nối PLC còn hoạt động hay không
+        private bool IsPlcConnected()
+        {
+            if (plc_s7_1200 != null && plc_s7_1200.IsConnected)
+            {
+                lblStatus.Text = "Trạng thái: Đã kết nối"; // Cập nhật trạng thái hiển thị
+                return true; // Trả về true nếu PLC đang kết nối
+            }
+            else
+            {
+                lblStatus.Text = "Trạng thái: Không kết nối"; // Cập nhật trạng thái khi mất kết nối
+                MessageBox.Show("PLC không còn kết nối."); // Hiển thị thông báo lỗi
+                return false; // Trả về false nếu PLC không còn kết nối
+            }
+        }
+
+        // Sự kiện Click của nút "Ghi dữ liệu" (btnWrite) để ghi dữ liệu xuống PLC
+        private void btnWrite_Click_1(object sender, EventArgs e)
+        {
+            // Kiểm tra xem PLC có còn kết nối không trước khi thực hiện ghi
+            if (IsPlcConnected())
+            {
+                try
+                {
+                    // Lấy dữ liệu từ TextBox (chuỗi nhập liệu) và chuyển thành số nguyên
+                    int dataToWrite = int.Parse(txtWriteData.Text);
+                    // Ghi dữ liệu xuống địa chỉ nhớ "MW0" của PLC
+                    plc_s7_1200.Write("DB1.DBW6", dataToWrite);
+
+                    MessageBox.Show("Đã ghi dữ liệu thành công xuống PLC!"); // Thông báo ghi thành công
+                }
+                catch (Exception exp)
+                {
+                    // Bắt lỗi nếu có lỗi khi thực hiện ghi dữ liệu
+                    MessageBox.Show("Ghi dữ liệu thất bại: " + exp.Message); // Hiển thị lỗi ghi
+                }
+            }
+        }
+
+        // Sự kiện Click của nút "Đọc dữ liệu" (btnRead) để đọc dữ liệu từ PLC
+        private void btnRead_Click_1(object sender, EventArgs e)
+        {
+            // Kiểm tra xem PLC có còn kết nối không trước khi thực hiện đọc
+            if (IsPlcConnected())
+            {
+                try
+                {
+                    // Đọc dữ liệu từ PLC dưới dạng 32-bit (4 bytes)
+                    var readData = plc_s7_1200.Read("DB1.DBW0");
+
+                    // Hiển thị dữ liệu đã đọc vào TextBox
+                    txtReadData.Text = readData.ToString();
+                }
+                catch (Exception exp)
+                {
+                    // Bắt lỗi nếu có lỗi khi thực hiện đọc dữ liệu
+                    MessageBox.Show("Đọc dữ liệu thất bại: " + exp.Message); // Hiển thị lỗi đọc
+                }
+            }
+        }
+
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (plc_s7_1200 != null && plc_s7_1200.IsConnected)
+                {
+                    plc_s7_1200.Close(); // Ngắt kết nối với PLC
+                    lblStatus.Text = "Trạng thái: Đã ngắt kết nối"; // Cập nhật trạng thái
+                    MessageBox.Show("Đã ngắt kết nối với PLC!");
+                }
+                else
+                {
+                    MessageBox.Show("PLC đã ngắt kết nối hoặc chưa kết nối.");
+                }
+            }
+            catch (Exception exp)
+            {
+                lblStatus.Text = "Trạng thái: Lỗi khi ngắt kết nối";
+                MessageBox.Show("Lỗi khi ngắt kết nối: " + exp.Message); // Hiển thị lỗi ngắt kết nối
+            }
+        }
+
+        private void btnReadFloat_Click(object sender, EventArgs e)
+        {
+            if (IsPlcConnected())
+            {
+                try
+                {
+                    // Đọc dữ liệu từ PLC dưới dạng 32-bit (4 bytes)
+                    uint readData = (uint)plc_s7_1200.Read("DB1.DBD8");
+
+                    // Chuyển đổi 4 bytes thành float
+                    byte[] dataToWrite = BitConverter.GetBytes(readData);
+                    float outf = BitConverter.ToSingle(dataToWrite, 0);
+
+                    // Hiển thị dữ liệu đã đọc vào TextBox
+                    txtReadData.Text = outf.ToString();
+                }
+                catch (Exception exp)
+                {
+                    // Bắt lỗi nếu có lỗi khi thực hiện đọc dữ liệu
+                    MessageBox.Show("Đọc dữ liệu thất bại: " + exp.Message); // Hiển thị lỗi đọc
+                }
+            }
+        }
+
+        private void btnWriteFloat_Click(object sender, EventArgs e)
+        {
+            if (IsPlcConnected())
+            {
+                try
+                {
+                    // Lấy giá trị float từ TextBox
+                    float inputValue = float.Parse(txtWriteData.Text); // TextBox nhập dữ liệu
+
+                    // Chuyển đổi float thành 4 bytes
+                    byte[] floatBytes = BitConverter.GetBytes(inputValue);
+
+                    // Chuyển đổi 4 bytes thành uint để ghi vào PLC
+                    uint writeData = BitConverter.ToUInt32(floatBytes, 0);
+
+                    // Ghi dữ liệu vào PLC
+                    plc_s7_1200.Write("DB1.DBD8", writeData);
+
+                    // Hiển thị thông báo thành công
+                    MessageBox.Show("Ghi dữ liệu thành công!");
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Giá trị nhập không hợp lệ! Vui lòng nhập một số thực.");
+                }
+                catch (Exception exp)
+                {
+                    // Bắt lỗi nếu có lỗi khi thực hiện ghi dữ liệu
+                    MessageBox.Show("Ghi dữ liệu thất bại: " + exp.Message); // Hiển thị lỗi ghi
+                }
+            }
+        }
+    }
+}
